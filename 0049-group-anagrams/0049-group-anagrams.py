c717ef3fc7c3cdcf17da76370ef5c3ce95fd1439
muskan9328-source <avii9328@gmail.com>
2026-03-16 22:25:52 +0530

    Time: 19 ms (80.35%), Space: 16.3 MB (46.53%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        hmap = {}
+        for word in strs:
+            key = "".join(sorted(word))
+            if key not in hmap:
+                hmap[key] = []
+            hmap[key].append(word)
+        return list(hmap.values())
+        
