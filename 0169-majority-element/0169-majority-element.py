ac0d1b894493fe22f66b05795e0ad712e6ba319e
muskan9328-source <avii9328@gmail.com>
2026-04-28 22:13:48 +0530

    Time: 8 ms (62.34%), Space: 13.7 MB (18%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -6,9 +6,11 @@ class Solution(object):
         for num in nums:
             if count == 0:
                 candidate = num
+            
             if num == candidate:
                 count += 1
             else:
                 count -= 1
-                
+        
         return candidate
+        
