65fa567e2e36914349a417d141e8daec3288b08f
muskan9328-source <avii9328@gmail.com>
2026-05-28 09:05:50 +0530

    Time: 223 ms (66.15%), Space: 27.7 MB (67.82%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        
+        if not grid:
+            return 0
+        
+        rows = len(grid)
+        cols = len(grid[0])
+        count = 0
+
+        def dfs(r, c):
+            # Boundary check
+            if r < 0 or c < 0 or r >= rows or c >= cols or grid[r][c] == "0":
+                return
+            
+            # Mark visited
+            grid[r][c] = "0"
+            
+            # Visit all 4 directions
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1":
+                    count += 1
+                    dfs(r, c)
+
+        return count
